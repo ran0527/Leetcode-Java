5b08f7f6f195c06ca252879fbf9fcac990e5f11b
Xinran <lindashi527@gmail.com>
2015-12-15 15:54:26 -0800

    Add 4Sum

---
+++ 4Sum.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        HashSet<List<Integer>> hashSet = new HashSet<List<Integer>>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        ArrayList<Integer> temp = new ArrayList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[left]);
+                        temp.add(nums[right]);
+                        if (!hashSet.contains(temp)) {
+                            hashSet.add(temp);
+                            res.add(temp);
+                        }
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
