b6ba2b59e4844dd81436b17ace77686d970e59e2
Xinran <lindashi527@gmail.com>
2015-12-20 18:50:33 -0800

    Add Merge Sorted Array

---
+++ Merge Sorted Array.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        m--;
+        n--;
+        while (m >= 0 || n >= 0) {
+            if (n < 0 || (m >= 0 && nums1[m] > nums2[n])) {
+                nums1[m + n + 1] = nums1[m];
+                m--;
+            } else {
+                nums1[m + n + 1] = nums2[n];
+                n--;
+            }
+        }
+        return;
+    }
+}
