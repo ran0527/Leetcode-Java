8f6605bd390c129786174a02cc76e019049ab07c
Xinran <lindashi527@gmail.com>
2016-01-07 16:49:00 -0800

    Add Find the Duplicate Number

---
+++ Find the Duplicate Number.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        int low = 1;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid) {
+                    count++;
+                }
+            }
+            if (count > mid) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
