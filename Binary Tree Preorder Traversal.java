77769b49fb833c427ed138b78591c02b56e6ec92
Xinran <lindashi527@gmail.com>
2016-02-01 19:32:01 -0800

    Add Iterative Sol

--- Binary Tree Preorder Traversal.java
+++ Binary Tree Preorder Traversal.java
@@ -22,3 +22,28 @@ public class Solution {
         helper(root.right, res);
     }
 }
+
+// Iterative
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            res.add(temp.val);
+
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+        return res;
+    }
+}
