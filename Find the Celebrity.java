26b2216d8e401613b0ae9f2090597fa53bc6ddaf
Xinran <lindashi527@gmail.com>
2016-02-03 14:56:21 -0800

    Add Find the Celebrity

---
+++ Find the Celebrity.java
@@ -0,0 +1,27 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        if (n <= 1) {
+            return -1;
+        }
+        int[] inDegree = new int[n];
+        int[] outDegree = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && knows(i, j)) {
+                    outDegree[i]++;
+                    inDegree[j]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
