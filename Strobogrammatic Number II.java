536ddad284333a078585f8de8165a4035a39a593
Xinran <lindashi527@gmail.com>
2016-02-05 10:22:00 -0800

    Add Strobogrammatic Number II

---
+++ Strobogrammatic Number II.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public List<String> findStrobogrammatic(int n) {
+        ArrayList<String> res = new ArrayList<String>();
+        helper(res, n, n);
+        return res;
+    }
+
+    private void helper(List<String> list, int n, int m) {
+        if (n == 0) {
+            list.add("");
+            return;
+        }
+        if (n == 1) {
+            list.add("0");
+            list.add("1");
+            list.add("8");
+            return;
+        }
+        helper(list, n - 2, m);
+        int size = list.size();
+        int i = 0;
+        while (i < size) {
+            String cur = list.get(i);
+            if (n != m) {
+                list.add("0" + cur + "0");
+            }
+            list.add("1" + cur + "1");
+            list.add("6" + cur + "9");
+            list.add("8" + cur + "8");
+            list.add("9" + cur + "6");
+            list.remove(0);
+            size--;
+        }
+    }
+}
