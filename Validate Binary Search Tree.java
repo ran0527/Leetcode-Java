5a9592244b615848b12dc9ac08b397260e62b48d
Xinran <lindashi527@gmail.com>
2016-02-09 11:53:11 -0800

    Add Sol

--- Validate Binary Search Tree.java
+++ Validate Binary Search Tree.java
@@ -22,3 +22,29 @@ public class Solution {
         return helper(p.left, p.val, min) && helper(p.right, max, p.val);
     }
 }
+
+# 2
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+
+    private boolean helper(TreeNode p, long max, long min) {
+        if (p == null) {
+            return true;
+        }
+        if ((p.val <= min) || (p.val >= max)) {
+            return false;
+        }
+        return helper(p.left, p.val, min) && helper(p.right, max, p.val);
+    }
+}
