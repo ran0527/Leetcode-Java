a19ff98d062f4696d541a230b07d1e81c1c8c139
Xinran <lindashi527@gmail.com>
2016-02-09 16:11:09 -0800

    Add Flip Game II

---
+++ Flip Game II.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean canWin(String s) {
+        if (s == null || s.length() < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.startsWith("++", i)) {
+                String t = s.substring(0, i) + "--" + s.substring(i + 2);
+                if (!canWin(t)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
